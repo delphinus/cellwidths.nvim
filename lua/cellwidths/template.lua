22c33c3e220c4646df128334d2c94e0274fe2958
delphinus <me@delphinus.dev>
2022-08-31 18:11:11 +0900

    Add user template feature

--- lua/cellwidths/template.lua
+++ lua/cellwidths/template.lua
@@ -1,4 +1,9 @@
-local parent_dir = (function()
+---@class cellwidths.template.LoadResult
+---@field cw_table cellwidths.table.CellWidthTable
+---@field clean_up boolean
+---@field save boolean
+
+local lib_dir = (function()
   local source = debug.getinfo(2, "S").source
   local dir = source:gsub("@(.*)%/[^%/]+$", "%1")
   return dir
@@ -10,23 +15,19 @@ end)()
 ---@field name string
 ---@field _path string
 local Template = {
-  dir = parent_dir,
+  lib_dir = lib_dir,
 }
 
 ---@param nvim cellwidths.nvim.Nvim
 ---@param name string
 ---@return cellwidths.template.Template
 Template.new = function(nvim, name)
-  local self = setmetatable({
-    nvim = nvim,
-    name = name,
-  }, { __index = Template })
-  return self
+  return setmetatable({ nvim = nvim, name = name }, { __index = Template })
 end
 
 ---@return string
 function Template:path()
-  self._path = self._path or self.dir .. "/templates/" .. self.name .. ".lua"
+  self._path = self._path or self.lib_dir .. "/templates/" .. self.name .. ".lua"
   return self._path
 end
 
@@ -35,51 +36,66 @@ function Template:exists()
   return not not self.nvim.uv.fs_stat(self:path())
 end
 
----@return table|nil
+---@return cellwidths.template.LoadResult?
 function Template:load()
   if not self:exists() then
-    self.nvim.log:debug("template: %s not found", self.name)
-    return
+    self.nvim.log:trace("template %s not found", self.name)
+    return nil
   end
-  local ok, code = pcall(require, "cellwidths.templates." .. self.name)
+  local cw_table = self:load_file(self.name)
+  if not cw_table then
+    self.nvim.log:trace("load template %s failed", self.name)
+    return nil
+  end
+  return { cw_table = cw_table, clean_up = true, save = false }
+end
+
+---@return table?
+function Template:load_file(name)
+  local ok, code = pcall(require, "cellwidths.templates." .. name)
   if not ok then
-    self.nvim.log:debug("requiring template: %s failed", self.name)
-    return
+    self.nvim.log:trace "requiring failed"
+    return nil
   end
-  local f, err = loadstring(code, self.name)
+  local f = loadstring(code, self.name)
   if not f then
-    self.nvim.log:debug("loadstring template: %s failed: %s", self.name, err)
-    return
+    self.nvim.log:trace "loadstring() failed"
+    return nil
   end
   local tbl = f()
   if type(tbl) ~= "table" then
-    self.nvim.log:debug("loaded result is not a table: %s", self.name)
-    return
+    self.nvim.log:trace "result from loadstring() is not a table"
+    return nil
   end
   return tbl
 end
 
----@param tbl table
----@return boolean
-function Template:save(tbl)
+---@param cw_table cellwidths.table.CellWidthTable
+---@return string?
+function Template:save(cw_table)
   local fd = self.nvim.uv.fs_open(self:path(), "w", tonumber("644", 8))
   if not fd then
-    self.nvim.log:debug("cannot open file: %s", self:path())
-    return false
+    return "cannot open file: " .. self:path()
   end
-  local f, err = load("return " .. vim.inspect(tbl))
+  local f, err = load("return " .. vim.inspect(self:create_data(cw_table)))
   if not f then
-    self.nvim.log:debug("failed to create func: %s", err)
-    return false
+    return "failed to create func: " .. err
   end
   local code = string.dump(f, true)
   local result = self.nvim.uv.fs_write(fd, "return " .. vim.inspect(code))
   if type(result) ~= "number" then
-    self.nvim.log:debug("cannot write code: %s", tostring(result))
-    return false
+    return "cannot write code: " .. tostring(result)
   end
   self.nvim.uv.fs_close(fd)
-  return true
+  self.nvim.log:trace("successfully saved template: %s", self.name)
+  return nil
+end
+
+---@param cw_table table
+---@return table
+-- luacheck: ignore self
+function Template:create_data(cw_table)
+  return { cw_table = cw_table }
 end
 
 ---@return nil
--- lua/cellwidths/user_template.lua
+++ lua/cellwidths/user_template.lua
@@ -1,17 +1,89 @@
+---@class Opts
+---@field listchars { [string]: string }
+---@field fillchars { [string]: string }
+
+---@class cellwidths.user_template.UserTemplateTable
+---@field opts Opts
+---@field cw_table cellwidths.table.CellWidthTable
+
 local Template = require "cellwidths.template"
 
 ---@class cellwidths.user_template.UserTemplate: cellwidths.template.Template
-local UserTemplate = {}
+---@field fallback fun(): cellwidths.table.CellWidthTable
+local UserTemplate = setmetatable({}, { __index = Template })
 
 ---@param nvim cellwidths.nvim.Nvim
----@name string
+---@param name string
+---@param fallback fun(): nil
 ---@return cellwidths.user_template.UserTemplate
-UserTemplate.new = function(nvim, name)
-  local self = setmetatable({
-    nvim = nvim,
-    name = "user/" .. name,
-  }, { __index = Template })
-  return self ---[[@as cellwidths.user_template.UserTemplate]]
+UserTemplate.new = function(nvim, name, fallback)
+  return setmetatable({ nvim = nvim, name = name, fallback = fallback }, { __index = UserTemplate })
+end
+
+---@return cellwidths.template.LoadResult
+function UserTemplate:load()
+  ---@return cellwidths.user_template.UserTemplateTable?
+  local function load_template()
+    if not self:exists() then
+      self.nvim.log:trace("%s does not exist", self.name)
+      return nil
+    end
+    local tbl = self:load_file(self.name:gsub("%/", ".", 1))
+    if not tbl then
+      self.nvim.log:trace "load_file failed"
+      return nil
+    end
+    if self:has_diff(tbl.opts) then
+      self.nvim.log:trace "has diff"
+      self.nvim.log:trace(tbl.opts)
+      return nil
+    end
+    return tbl
+  end
+
+  local tbl = load_template()
+  return tbl and { cw_table = tbl.cw_table, clean_up = false, save = false }
+      or { cw_table = self.fallback(), clean_up = true, save = true }
+end
+
+---@param opts Opts?
+---@return boolean
+function UserTemplate:has_diff(opts)
+  ---@param orig any
+  ---@param new any
+  ---@return boolean
+  local function has_diff(orig, new)
+    if type(orig) ~= "table" or type(new) ~= "table" then
+      return false
+    end
+    for k, v in pairs(orig) do
+      if v ~= new[k] then
+        return true
+      end
+    end
+    return false
+  end
+
+  local old_opts = opts or {}
+  local new_opts = self:opts()
+  return has_diff(old_opts.listchars, new_opts.listchars) or has_diff(old_opts.fillchars, new_opts.fillchars)
+end
+
+---@param cw_table cellwidths.table.CellWidthTable
+---@return cellwidths.user_template.UserTemplateTable
+function UserTemplate:create_data(cw_table)
+  return {
+    opts = self:opts(),
+    cw_table = cw_table,
+  }
+end
+
+---@return Opts
+function UserTemplate:opts()
+  return {
+    listchars = self.nvim.opt.listchars:get(),
+    fillchars = self.nvim.opt.fillchars:get(),
+  }
 end
 
 return UserTemplate
